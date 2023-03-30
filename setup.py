3d4c3e141064db4b082dcc326e81d0ff5887f23a
yagi <121270079+yagisann@users.noreply.github.com>
2023-03-30 19:50:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AutoInventory',
-    version='1.0.2',
+    version='1.0.3',
     description='棚卸の自動化コード、IASOに対応',
     author="yagisann",
     packages=find_packages(),
